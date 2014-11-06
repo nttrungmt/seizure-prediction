502d7920e86d59e6542f4e02f3bdc820cd45eed3
Ubuntu <ubuntu@ip-10-171-94-80.ec2.internal>
2014-11-06 13:55:24 +0000

    141106-predict.1

---
+++ dodata1.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# D5 D1 D2 D4 D3 P1 P2
+
+python seizure_detection.py -b td -t Dog_4 &> log/Dog_4_td &
+python seizure_detection.py -b tt -t Dog_4 &> log/Dog_4_tt &
+
+python seizure_detection.py -b td -t Dog_3 &> log/Dog_3_td &
+python seizure_detection.py -b tt -t Dog_3 &> log/Dog_3_tt &
+
+python seizure_detection.py -b td -t Patient_1 &> log/Patient_1_td &
+python seizure_detection.py -b td -t Dog_5 &> log/Dog_5_td &
+python seizure_detection.py -b td -t Dog_1 &> log/Dog_1_td &
+python seizure_detection.py -b tt -t Patient_1 &> log/Patient_1_tt &
+python seizure_detection.py -b tt -t Dog_5 &> log/Dog_5_tt &
+python seizure_detection.py -b tt -t Dog_1 &> log/Dog_1_tt &
+
+
+python seizure_detection.py -b td -t Patient_2 &> log/Patient_2_td &
+python seizure_detection.py -b tt -t Patient_2 &> log/Patient_2_tt &
+python seizure_detection.py -b td -t Dog_2 &> log/Dog_2_td &
+python seizure_detection.py -b tt -t Dog_2 &> log/Dog_2_tt &
+
